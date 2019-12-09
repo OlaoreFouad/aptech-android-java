8419be2718ae98d44e34640ee486a3e43b8ab912
Foodie <olaorefouad565@gmail.com>
2019-12-09 11:06:13 +0100

    we worked on the overflow menu

--- NavigationWithFragments/app/src/main/java/dev/iamfoodie/navigationwithfragments/MainActivity.java
+++ NavigationWithFragments/app/src/main/java/dev/iamfoodie/navigationwithfragments/MainActivity.java
@@ -1,10 +1,14 @@
 package dev.iamfoodie.navigationwithfragments;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import dev.iamfoodie.navigationwithfragments.fragments.OneFragment;
@@ -19,7 +23,6 @@ public class MainActivity extends AppCompatActivity implements OnDataSentListene
         setContentView(R.layout.activity_main);
 
         init();
-
     }
 
     @Override
@@ -38,6 +41,37 @@ public class MainActivity extends AppCompatActivity implements OnDataSentListene
     public void init() {
         OneFragment oneFragment = new OneFragment();
         setFragment(oneFragment, "fragmentOne", false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_contact:
+                Toast.makeText(this, "Contacts Clicked!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_about:
+                Toast.makeText(this, "About Clicked!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings Clicked!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_search:
+                Toast.makeText(this, "Search clicked!", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 
     public void setFragment(Fragment fragment, String tag, boolean addToBackStack) {
