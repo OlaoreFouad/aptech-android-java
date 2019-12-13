885174d8f85d10f6e65049a9dc6b5e39b048818c
Foodie <olaorefouad565@gmail.com>
2019-12-13 17:06:50 +0100

    made changes to the chats fragment

--- TabLayoutDemo/app/src/main/java/dev/iamfoodie/tablayoutdemo/fragments/ChatsFragment.java
+++ TabLayoutDemo/app/src/main/java/dev/iamfoodie/tablayoutdemo/fragments/ChatsFragment.java
@@ -16,29 +16,9 @@ import dev.iamfoodie.tablayoutdemo.listeners.OnFragmentNavigatedListener;
 
 public class ChatsFragment extends Fragment {
 
-//    private Button nextButton;
-//    private OnFragmentNavigatedListener mOnFragmentNavigatedListener;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_chats, container, false);
     }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        /*nextButton = view.findViewById(R.id.next_button);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mOnFragmentNavigatedListener.nextFragment("fragmentStatus");
-            }
-        });*/
-    }
-
-    /*@Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        mOnFragmentNavigatedListener = (OnFragmentNavigatedListener) context;
-    }*/
 }
